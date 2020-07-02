4f2a74a7e0a2fd3c484ff985ac5d0f7ca89517d2
Stefan Hagen <stefan@hagen.link>
2020-07-02 22:18:25 +0200

    Enhanced testability of the schema validation tool by allowing the schema text and document text to be embedded

--- validation/schema_validation.py
+++ validation/schema_validation.py
@@ -5,7 +5,7 @@
 import json
 import sys
 
-from jsonschema import draft7_format_checker, validate  # type: ignore
+import jsonschema  # type: ignore
 
 ENCODING = "utf-8"
 
@@ -16,16 +16,23 @@ def load(file_path):
         return json.load(handle)
 
 
-def main(argv=None):
+def validate(document, schema, conformance=None):
+    """Validate the document against the schema."""
+    conformance = conformance if conformance else jsonschema.draft7_format_checker
+    return jsonschema.validate(document, schema, format_checker=conformance)
+
+
+def main(argv=None, embedded=False):
     """Drive the validation."""
     argv = argv if argv else sys.argv[1:]
     if len(argv) != 2:
         print("Validation requires two positional arguments: schema.json document.json")
         return 2
 
-    json_schema_path, json_document_path = argv
-    validate(load(json_document_path), load(json_schema_path), format_checker=draft7_format_checker)
-    return 0
+    schema = json.loads(argv[0]) if embedded else load(argv[0])
+    document = json.loads(argv[1]) if embedded else load(argv[1])
+
+    return 0 if validate(document, schema) else 1
 
 
 if __name__ == "__main__":
