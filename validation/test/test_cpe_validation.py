70fb17cd6b9000bf13701be5e9f9479d274f557f
Stefan Hagen <stefan@hagen.link>
2020-07-04 00:03:45 +0200

    Added some test cases for cpe validation

---
+++ validation/test/test_cpe_validation.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=import-error,missing-docstring,unused-import,reimported
+import json
+import pytest  # type: ignore
+import cpe_validation
+
+
+def test_cli_encoding_ok():
+    assert cpe_validation.ENCODING == "utf-8"
+
+
+def test_cli_main_ok():
+    assert 0 == cpe_validation.main(["cpe:/"], True)
+
+
+def test_cli_main_nok_user():
+    assert 2 == cpe_validation.main([], True)
+
+
+def test_cli_main_nok_data():
+    assert 1 == cpe_validation.main(["{"], True)
+
+
+def test_cli_main_nok_file():
+    message = r"\[Errno 2\] No such file or directory: '{'"
+    with pytest.raises(FileNotFoundError, match=message):
+        cpe_validation.main(["{"], False)
